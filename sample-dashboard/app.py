8b522d0bc47d1ed6c5cc382998ac8fbb105e6bc0
yuto <44987057+yutowac@users.noreply.github.com>
2024-07-25 09:28:26 +0900

    Update app.py

--- sample-dashboard/app.py
+++ sample-dashboard/app.py
@@ -4,6 +4,7 @@ from io import StringIO
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # Load the data
 def load_original_data():
